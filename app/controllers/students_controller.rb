b04a1bcdd9088296eeb4c4d8cd95e128189cfccc
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-04-09 11:20:49 -0700

    Added flash and error messages for student signup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have succesfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
 end
